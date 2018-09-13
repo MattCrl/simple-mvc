cf332aca315c27d2f33b4fe6450ffe1d6ae3d6d7
Matt Crl <matth.carlevaris@gmail.com>
2018-09-13 23:45:29 +0200

    First quete MVC

--- src/Model/ItemManager.php
+++ src/Model/ItemManager.php
@@ -2,7 +2,7 @@
 // src/Model/ItemManager.php
 require __DIR__ . '/../../app/db.php';
 
-// récupération de tous les items
+// Récupération de tous les items
 function selectAllItems() :array
 {
     $pdo = new \PDO(DSN, USER, PASS);
